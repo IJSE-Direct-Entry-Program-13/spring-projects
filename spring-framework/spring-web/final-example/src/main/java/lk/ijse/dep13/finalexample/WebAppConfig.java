555b85f1f7cd60e68d17e958a65140d0ab1bfed0
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-11 16:00:14 +0530

    Finish final example

--- spring-framework/spring-web/final-example/src/main/java/lk/ijse/dep13/finalexample/WebAppConfig.java
+++ spring-framework/spring-web/final-example/src/main/java/lk/ijse/dep13/finalexample/WebAppConfig.java
@@ -1,11 +1,23 @@
 package lk.ijse.dep13.finalexample;
 
+import lk.ijse.dep13.finalexample.interceptor.MyInterceptor1;
+import lk.ijse.dep13.finalexample.interceptor.MyInterceptor2;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @ComponentScan
 @Configuration
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor1())
+                .addPathPatterns("/**");
+        registry.addInterceptor(new MyInterceptor2())
+                .addPathPatterns("/**");
+    }
 }
--- spring-framework/spring-web/final-example/src/main/java/lk/ijse/dep13/finalexample/api/MyController.java
+++ spring-framework/spring-web/final-example/src/main/java/lk/ijse/dep13/finalexample/api/MyController.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep13.finalexample.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,10 +9,11 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/hello2")
 public class MyController {
 
+    @GetMapping
     public ModelAndView hello(){
         System.out.println("MyController:hello()");
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("hello");
+        mv.setViewName("hello.jsp");
         mv.addObject("id", "C001");
         mv.addObject("name", "Kasun");
         mv.addObject("address", "Galle");
